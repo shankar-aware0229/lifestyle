cf3e1932f549d70f717b6072239d06b80435705c
Shankar Aware <shankar.aware09@gmail.com>
2019-11-02 18:28:45 +0530

    Adding Network Test cases
    Checking for network request with different test cases.

---
+++ Lifestyle/LifestyleTests/NetworkTests.swift
@@ -0,0 +1,64 @@
+//
+//  NetworkTests.swift
+//  Lifestyle
+//
+//  Created by SHANKAR AWARE on 02/11/19.
+//  Copyright © 2019 Shankar Aware. All rights reserved.
+//
+
+import XCTest
+@testable import Lifestyle
+
+class NetworkTests: XCTestCase {
+    func test_whenWrongURLIsProvidedNetworkWillFail() {
+        let network = NetworkCategoryRepository(url: nil)
+        
+        let exp = expectation(description: "Done")
+        
+        network.getCategoriesDetails { result in
+            let error = result.error
+            
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error?.localizedDescription, "URL is incorrect")
+            
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10.0, handler: nil)
+    }
+    
+    func test_networkReturnsOnMainThread() {
+        let url: URL = URL(string: "https://www.google.com/givememymoney/you/idiot")!
+        let network = NetworkCategoryRepository(url: url)
+        
+        let exp = expectation(description: "Done")
+        
+        network.getCategoriesDetails { result in
+            
+            XCTAssertTrue(Thread.isMainThread)
+            
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10.0, handler: nil)
+    }
+    
+    func test_networkReturnsWithWrongDataAParsingErrorIsReturned() {
+        let url: URL = URL(string: "https://www.google.com")!
+        let network = NetworkCategoryRepository(url: url)
+        
+        let exp = expectation(description: "Done")
+        
+        network.getCategoriesDetails { result in
+            let error = result.error
+            
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error?.localizedDescription, ResponseError.wrongData.localizedDescription)
+
+            exp.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10.0, handler: nil)
+    }
+
+}
